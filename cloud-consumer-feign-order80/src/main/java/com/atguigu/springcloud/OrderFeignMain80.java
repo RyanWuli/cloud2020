2b03ca97c0e5900f01955398c24acdcc87167997
RyanWuli <972698998@qq.com>
2020-06-04 18:15:13 +0800

    openfeign

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -2,21 +2,22 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
 /**
- * @author: ChaW
- * @date: 2020/5/18
- * @time: 22:14
- * @description: No Description
- * @version: 1.0
+ * @Author: Ryan
+ * @Date: 2020/6/4 14:08
+ * @Version: 1.0
+ * @Description:
  */
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableDiscoveryClient
-public class PaymentMain8001 {
+@EnableFeignClients
+public class OrderFeignMain80 {
+
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(OrderFeignMain80.class, args);
     }
 }
---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @Author: Ryan
+ * @Date: 2020/6/4 14:13
+ * @Version: 1.0
+ * @Description:
+ */
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+}
